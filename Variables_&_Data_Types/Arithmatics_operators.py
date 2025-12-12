7afff395b66e020902767909545918e846ba930a
Mohd Shakib Saifi <moshakibsaifi03@gmail.com>
2025-12-12 17:43:42 +0530

    Arithmetics operators

---
+++ Variables_&_Data_Types/Arithmatics_operators.py
@@ -0,0 +1,12 @@
+#arithmatics operators
+a = 10
+b = 3
+
+print(a + b)   
+print(a - b)    
+print(a * b)  
+print(a / b)  # float division
+print(a // b)  # floor division
+print(a % b)   # modulus
+print(a ** b)  # exponentiation
+# --- IGNORE ---
